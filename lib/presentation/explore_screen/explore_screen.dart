cbc339be033e5feb8e34499af8d7bc16080d857b
zhouanqi <angiechou1231@gmail.com>
2026-04-19 17:47:24 +0300

    add the create button

--- lib/presentation/explore_screen/explore_screen.dart
+++ lib/presentation/explore_screen/explore_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../../core/app_export.dart';
+import '../../widgets/custom_floating_action_button.dart';
+import '../../widgets/custom_image_view.dart';
 import '../recipe_search_results_screen/widgets/recipe_card_item.dart';
 import './controller/explore_controller.dart';
 
@@ -15,25 +17,52 @@ class ExploreScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: appTheme.white_A700,
-      body: Obx(
-        () => ListView.separated(
-          // Added horizontal padding (16.h) between cards and screen edge
-          padding: EdgeInsets.fromLTRB(16.h, statusBarHeight + 20.h, 16.h, 20.h),
-          itemCount: controller.exploreModelObj.value.recipeList?.length ?? 0,
-          separatorBuilder: (context, index) => SizedBox(height: 16.h),
-          itemBuilder: (context, index) {
-            var recipe = controller.exploreModelObj.value.recipeList?[index];
-            return RecipeCardItem(
-              recipeItemModel: recipe,
-              onCardTap: () {
-                Get.toNamed(AppRoutes.recipeDetailScreen);
+      body: Stack(
+        children: [
+          Obx(
+            () => ListView.separated(
+              // Added horizontal padding (16.h) between cards and screen edge
+              padding: EdgeInsets.fromLTRB(16.h, statusBarHeight + 20.h, 16.h, 20.h),
+              itemCount: controller.exploreModelObj.value.recipeList?.length ?? 0,
+              separatorBuilder: (context, index) => SizedBox(height: 16.h),
+              itemBuilder: (context, index) {
+                var recipe = controller.exploreModelObj.value.recipeList?[index];
+                return RecipeCardItem(
+                  recipeItemModel: recipe,
+                  onCardTap: () {
+                    Get.toNamed(AppRoutes.recipeDetailScreen);
+                  },
+                  onBookmarkTap: () {
+                    controller.toggleBookmark(index);
+                  },
+                );
               },
-              onBookmarkTap: () {
-                controller.toggleBookmark(index);
-              },
-            );
-          },
-        ),
+            ),
+          ),
+          _buildFloatingActionButtons(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFloatingActionButtons(BuildContext context) {
+    return Positioned(
+      bottom: 24.h,
+      right: 16.h,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CustomFloatingActionButton(
+            onPressed: () {
+              Get.toNamed(AppRoutes.recipeCreationScreen);
+            },
+            child: CustomImageView(
+              imagePath: ImageConstant.imgCreateARecipe,
+              color: appTheme.white_A700,
+            ),
+            backgroundColor: appTheme.deep_purple_800,
+          ),
+        ],
       ),
     );
   }
